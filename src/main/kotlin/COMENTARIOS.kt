dcf5cc3f0885aa2d20979c3436aee9047b1136f7
X1T4D1N <pedroinocencio271@gmail.com>
2023-01-11 21:26:34 -0300

    first commit

---
+++ src/main/kotlin/COMENTARIOS.kt
@@ -0,0 +1,24 @@
+/**
+ * Type  Bit
+ * Double 64
+ * Float 32
+ * Long  64
+ * Int   32
+ *  Byte 8
+ * Boolean ?
+ * String ?
+ * Char ?
+ */
+// +(SOMA),-(SUBTRAÇÃO),*(MULTIPLICAÇÃO),/(DIVISÃO),%(RESTO)
+
+//DESAFIO
+//fun main() {
+//    var nome: String = "pedro inocencio"
+//    var textsemUso: String
+//    var Calcado: Int = 45
+//    var PIB: Long = 1869000000000
+//    var POP: Int = 211000000
+//
+//    println("R$ ${PIB+POP}")
+//}
+
