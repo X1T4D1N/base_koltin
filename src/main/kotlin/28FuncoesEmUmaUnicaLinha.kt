0883119b9ed39d5b4bb392254ade2ed05117e415
pedroinocencio <pedroinocencio271@gmail.com>
2023-01-12 20:06:12 -0300

    funcoes em uma unica linha

---
+++ src/main/kotlin/28FuncoesEmUmaUnicaLinha.kt
@@ -0,0 +1,16 @@
+fun main() {
+    //helloWorld()
+    //println("A soma de 10 e 5 é ${sum(10, 5)}")
+    println(divisao(10f, 3.5f))
+
+}
+
+fun sum(a: Int, b: Int): Int {
+    return (a + b)
+}
+
+fun sum2(a: Int, b: Int) = (a + b)
+
+fun helloWorld() = println("Hello, world!")
+
+fun divisao(a: Float, b: Float) = a / b
